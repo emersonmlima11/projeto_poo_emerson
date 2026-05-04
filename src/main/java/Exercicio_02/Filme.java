60400c72ad4468f6a53ec105b89f3cf2dbda4fee
emersonmlima11 <emerson.mlima11@gmail.com>
2026-05-04 20:49:01 -0300

    Criação do projeto padrão e da classe Filme

---
+++ src/main/java/Exercicio_02/Filme.java
@@ -0,0 +1,104 @@
+package Exercicio_02;
+
+import java.util.Objects;
+
+public class Filme {
+    int codigo, duracao, quantidadeEmEstoque;
+    String titulo, genero;
+    boolean disponivel = true;
+
+    public Filme(int codigo, String titulo, String genero, int quantidadeEmEstoque, int duracao){
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.genero = genero;
+        this.duracao = duracao;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+
+    public Filme(int codigo, String titulo, String genero, int duracao){
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.genero = genero;
+        this.duracao = duracao;
+        this.quantidadeEmEstoque = 1;
+    }
+
+    public Filme(String titulo, String genero, int quantidadeEmEstoque, int duracao){
+        this.titulo = titulo;
+        this.genero = genero;
+        this.duracao = duracao;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+
+    public Filme(String titulo, String genero, int duracao){
+        this.titulo = titulo;
+        this.genero = genero;
+        this.duracao = duracao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Filme filme = (Filme) o;
+
+        return codigo == filme.codigo;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(codigo);
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public int getQuantidadeEmEstoque() {
+        return quantidadeEmEstoque;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public void setQuantidadeEmEstoque(int quantidadeEmEstoque) {
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Título: "+this.titulo
+                +"; Genero: "+this.genero
+                +"; Duraçao: "+this.duracao
+                +"; Estoque: "+this.quantidadeEmEstoque;
+    }
+}
