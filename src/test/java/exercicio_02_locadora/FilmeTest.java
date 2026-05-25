45ea6a63202bb77cf043a09eebb3bfda9ca4b8b1
emerson.lima <emerson.mlima11@gmail.com>
2026-05-25 11:38:40 -0300

    Teste para Filme

--- src/test/java/exercicio_02_locadora/FilmeTest.java
+++ src/test/java/exercicio_02_locadora/FilmeTest.java
@@ -1,4 +1,19 @@
 package exercicio_02_locadora;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FilmeTest {
+
+    @Test
+    public void testConstrutores(){
+        Filme f1 = new Filme(1, "Jogos Vorazes", "Ação", 10, 125);
+        assertEquals(1, f1.getCodigo());
+        assertEquals("Jogos Vorazes", f1.getTitulo());
+        assertEquals("Ação", f1.getGenero());
+        assertEquals(10, f1.getQuantidadeEmEstoque());
+        assertEquals();
+    }
+
 }
